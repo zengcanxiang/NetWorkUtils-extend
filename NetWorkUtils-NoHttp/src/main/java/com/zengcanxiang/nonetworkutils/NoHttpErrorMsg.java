a6371ae1d35fd9c21cf98fb0322a85a40628fb87
zengcanxiang <zengcanxiang@163.com>
2016-03-10 22:35:09 +0800

    修改包含jar和依赖android版本的问题

--- NetWorkUtils-NoHttp/src/main/java/com/zengcanxiang/nonetworkutils/NoHttpErrorMsg.java
+++ NetWorkUtils-NoHttp/src/main/java/com/zengcanxiang/nonetworkutils/NoHttpErrorMsg.java
@@ -1,5 +1,14 @@
 package com.zengcanxiang.nonetworkutils;
 
+import com.yolanda.nohttp.error.ArgumentError;
+import com.yolanda.nohttp.error.ClientError;
+import com.yolanda.nohttp.error.NetworkError;
+import com.yolanda.nohttp.error.ServerError;
+import com.yolanda.nohttp.error.StorageReadWriteError;
+import com.yolanda.nohttp.error.StorageSpaceNotEnoughError;
+import com.yolanda.nohttp.error.TimeoutError;
+import com.yolanda.nohttp.error.URLError;
+import com.yolanda.nohttp.error.UnKnownHostError;
 import com.zengcanxiang.network.NetWorkError;
 
 /**
@@ -60,5 +69,31 @@ public class NoHttpErrorMsg extends NetWorkError {
 
     public void setNetworkMillis(long networkMillis) {
         this.networkMillis = networkMillis;
+    }
+
+    public String decideError(){
+        StringBuffer message=new StringBuffer();
+        if (exception instanceof ClientError) {
+            message.append("客户端错误");
+        } else if (exception instanceof ServerError) {
+            message .append( "服务器错误");
+        } else if (exception instanceof NetworkError) {
+            message .append( "网络不可用，请检查网络");
+        } else if (exception instanceof StorageReadWriteError) {
+            message .append( "存储卡错误，请检查存储卡");
+        } else if (exception instanceof StorageSpaceNotEnoughError) {
+            message .append( "存储位置空间不足");
+        } else if (exception instanceof TimeoutError) {
+            message .append( "请求超时");
+        } else if (exception instanceof UnKnownHostError) {
+            message .append( "找不到服务器");
+        } else if (exception instanceof URLError) {
+            message .append( "url地址错误");
+        } else if (exception instanceof ArgumentError) {
+            message .append( "请求参数错误");
+        } else {
+            message .append( "未知错误");
+        }
+        return message.toString();
     }
 }
